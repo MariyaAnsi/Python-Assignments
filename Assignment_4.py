4f8f24ccab605046423f4d52f50de757ab6d09fe
MariyaAnsi <72558504+MariyaAnsi@users.noreply.github.com>
2021-01-17 20:38:00 +0530

    Update Assignment_4.py

--- Assignment_4.py
+++ Assignment_4.py
@@ -1,4 +1,4 @@
-#Write code using find() and string slicing (see section 6.10)
+#Write code using find() and string slicing.
 #to extract the number at the end of the line below.
 #Convert the extracted value to a floating point number and print it out.
 
